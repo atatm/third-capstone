f4d688504a382b9c4a7661e8defdb5f13d2ed6d5
atatm <alelignayana@gmail.com>
2023-01-13 13:41:15 +0300

    Make the movie character profile

---
+++ third-capstone/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// stylesheet
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>2023 All Rights Reserved. </p>
+  </footer>
+);
+export default Footer;
---
+++ third-capstone/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faHome, faGear, faMicrophone } from '@fortawesome/free-solid-svg-icons';
+
+// stylesheet
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="navBar">
+    <ul className="menuNav">
+      <Link to="/">
+        <li>
+          <FontAwesomeIcon className="homeIcon" icon={faHome} />
+        </li>
+      </Link>
+      <li>
+        <FontAwesomeIcon className="micIcon" icon={faMicrophone} />
+        <FontAwesomeIcon className="gearIcon" icon={faGear} />
+      </li>
+    </ul>
+  </nav>
+);
+export default Navbar;
